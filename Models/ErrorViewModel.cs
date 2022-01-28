d3c7cdd6d4c1013722e7cf6bf47712c51026e791
Kai Tapper <kai.tapper@outlook.com>
2022-01-28 11:19:15 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Garage_2_Group_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
