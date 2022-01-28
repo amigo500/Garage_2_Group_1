050f5bf65fa9835ab0da1d7f6dd6f14023343208
Kai Tapper <kai.tapper@outlook.com>
2022-01-28 14:33:45 +0100

    Validation for RegId to ensure Correct Format.

---
+++ Utils/Validation.cs
@@ -0,0 +1,58 @@
+﻿namespace Garage_2_Group_1.Utils
+{
+    public class Validation
+    {
+        public Boolean RegIdValidation(string regID)
+        {
+            regID = regID.Trim();
+            regID = regID.ToUpper();
+
+            char[] validChars = "QWERTYUIOPASDFGHJKLZXCVBNM".ToCharArray();
+            if (regID.Length == 6)
+            {
+                bool valid;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (validChars.Contains(regID[i]))
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        valid = false;
+                        return false;
+                    }
+                }
+                for (int i = 3; i < 5; i++)
+                {
+                    if (int.TryParse(regID[i].ToString(), out _))
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        valid = false;
+                        return valid;
+                    }
+                }
+                if (validChars.Contains(regID[5]))
+                {
+                    valid = true;
+                }
+                else if (int.TryParse(regID[5].ToString(), out _))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+                return valid;
+            }
+            else
+            {
+                return false;
+            }   
+        }
+    }
+}
