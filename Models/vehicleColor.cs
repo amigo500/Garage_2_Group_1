c3db3bd0c03bb6ceea6e340cd511b01b06022531
Kai Tapper <kai.tapper@outlook.com>
2022-01-31 09:19:59 +0100

    added enum colors

--- Models/vehicleColor.cs
+++ Models/vehicleColor.cs
@@ -2,5 +2,17 @@
 {
     public enum VehicleColor
     {
+        Blue,
+        Red,
+        Green,
+        Black,
+        White,
+        Silver,
+        Brown,
+        Yellow,
+        Orange,
+        Purple,
+        Pink,
+        Grey
     }
 }
