a46f488e2e6fc4562acf8e8086315c1b44df0c92
amigo500 <alexhouser@hotmail.com>
2022-02-16 10:34:53 +0100

    commit UserCreated

--- Garage.MVC/Models/UserViewModels/UserCreateViewModel.cs
+++ Garage.MVC/Models/UserViewModels/UserCreateViewModel.cs
@@ -8,9 +8,15 @@ namespace Garage_2_Group_1.Models.UserViewModels
         [Required]
         [Display(Name = "Social Security Number")]
         public int SSN { get; set; }
+
         [Required]
+        [Display(Name = "First Name")]
+        [MinLength(2, ErrorMessage = "First name must be minimum two characters")]
         public string FirstName { get; set; }
+        
         [Required]
+        [Display(Name = "Last Name")]
+        [MinLength(2, ErrorMessage = "Last name must be minimum two characters")]
         public string LastName { get; set; }
         
 
