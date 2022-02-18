c2e925ae16f93dca9c09a314eca4eacc2a7a6d1a
Kai Tapper <kai.tapper@outlook.com>
2022-02-18 12:07:55 +0100

    Made a partial view for user details to show currently registered vehicles. Buttons don't work on the cards and it looks awful

--- Garage.MVC/Models/UserViewModels/UserDetailsViewModel.cs
+++ Garage.MVC/Models/UserViewModels/UserDetailsViewModel.cs
@@ -13,6 +13,12 @@ namespace Garage_2_Group_1.Models.UserViewModels
         public int RegisteredVehiclesAmount { get; set; }
         public string Avatar { get; set; }
         public IEnumerable<Vehicle> Vehicles { get; set; }
+        public long UserSSN { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public string RegNr { get; set; }
+        public VehicleType Type { get; set; }
 
 
     }
