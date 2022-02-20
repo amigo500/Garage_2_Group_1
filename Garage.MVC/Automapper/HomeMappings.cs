e43d70744a8ed21d79d170f18c3559d1342e3c73
Kai Tapper <kai.tapper@outlook.com>
2022-02-20 15:30:09 +0100

    Made a template for the new ParkatronDetailsViewModel aka LandingPage aka Stats Page. Added a mapper to go with it.

---
+++ Garage.MVC/Automapper/HomeMappings.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Garage.Entities;
+using Garage_2_Group_1.Models.UserViewModels;
+using Garage_2_Group_1.Models.VehicleVeiwModels;
+using Garage_2_Group_1.Models.ViewModels;
+
+namespace Garage_2_Group_1.Automapper
+{
+    public class HomeMappings : Profile
+    {
+        public HomeMappings()
+        {
+          
+        }
+    }
+}
---
+++ Garage.MVC/Models/ViewModels/ParkatronDetailsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Garage.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Garage_2_Group_1.Models.ViewModels
+#nullable disable  
+{
+    public class ParkatronDetailsViewModel
+    {
+        
+
+        public int NumberOfParkedVehicles { get; set; }
+        public int NumberOfRegisteredVehicles { get; set; }
+        public int NumberOfRegisteredUsers { get; set; }
+        public int ParkedVehicleGrossing { get; set; }
+        public int EarnedTotals { get; set; }
+        public int UsedParkingSlots { get; set; }
+        public int TotalGarageSpace { get; set; }
+        public IEnumerable<Vehicle> RegisteredVehicles { get; set; }
+        public IEnumerable<ParkingSlot> ParkingSlots { get; set; }
+        public IEnumerable<User> RegisteredUsers { get; set; }
+        public IEnumerable<VehicleType> RegisteredVehicleTypes { get; set; }
+
+
+
+
+
+    }
+}
